8b030eb74ced73a5038047374d119ec8f7203a42
Zuans <juandev.net@gmail.com>
2023-01-07 10:30:46 +0700

    change api key

--- src/scripts/globals/config.js
+++ src/scripts/globals/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  KEY: 'YOUR_API_KEY',
+  KEY: '1269d0819c6cd7229e26dbf2b69613a8',
   BASE_URL: 'https://api.themoviedb.org/3/',
   BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
   DEFAULT_LANGUAGE: 'en-us',
